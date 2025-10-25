a7e100ce1f2291b607e38cbf40d3faab038d77c7
Smatrace Solutions <smatracekenya@gmail.com>
2025-10-25 21:30:40 +0000

    createConsoleError@https://6000-firebase-studio-1761420968697.cluster-ik

--- src/components/layout/app-shell.tsx
+++ src/components/layout/app-shell.tsx
@@ -95,7 +95,7 @@ export function AppShell({ children }: { children: React.ReactNode }) {
           <SidebarMenu>
             {navItems.map((item) => (
               <SidebarMenuItem key={item.href}>
-                <Link href={item.href} legacyBehavior passHref>
+                <Link href={item.href} passHref>
                   <SidebarMenuButton
                     isActive={pathname === item.href}
                     tooltip={{ children: item.label }}
